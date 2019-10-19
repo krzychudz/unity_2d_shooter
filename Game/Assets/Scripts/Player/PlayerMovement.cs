075b122603188b4b7621b5f03b8a8afdb6ddfae0
Krzysztof <krzysztof.dziedzic175@gmail.com>
2019-10-19 19:26:30 +0200

    Add virtual joystick

---
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 10.0f;
+
+    protected Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerRigidbody2d = GetComponent<Rigidbody2D>();
+        playerRigidbody2d.velocity = new Vector2(joystick.Horizontal * movementSpeed * Time.deltaTime, joystick.Vertical * movementSpeed * Time.deltaTime);
+    }
+}
